21f5719c0b2d48fef23ea94aa499d6e5dbc60e3a
Paulo Lima <ph.lima1983@gmail.com>
2025-05-07 11:35:08 -0300

    Add healthcheck endpoint and update logging; modify requirements for fastapi and uvicorn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from fastapi import FastAPI
-from server import webhook, create_knowledge, create_prompt
+from server import webhook, create_knowledge, create_prompt, healthcheck
 from logs.logging_config import log_queue, start_log_processor
 
 asyncio.create_task(log_queue.put(f"Servidor iniciado..."))
@@ -11,3 +11,4 @@ app = FastAPI(on_startup=[start_log_processor])
 app.include_router(webhook.router)
 app.include_router(create_knowledge.router)
 app.include_router(create_prompt.router)
+app.include_router(healthcheck.router)
