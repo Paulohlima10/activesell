87c7f91218d16a2893da24e53ad3cb36e56fc93c
Paulo Lima <ph.lima1983@gmail.com>
2025-06-02 19:06:41 -0300

    Fix URL in send_text_via_http function and add test script for HTTP message sending

---
+++ testes/ts01.py
@@ -0,0 +1,21 @@
+import requests
+
+def send_text_via_http(phone_number, msg, msg_id="90B2F8B13FAC8A9CF6B06E99C7834DC5", token="9C84DC7EBCC6-4B17-8625-A4A60018AC03", url="http://52.23.198.211:8080/chat/send/text"):
+    headers = {
+        "Token": token,
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "Phone": phone_number,
+        "Body": msg,
+        "Id": msg_id
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    return response.json()
+
+
+telefone = "5534991704671"
+mensagem = "Olá, este é um teste de mensagem de texto via HTTP!"
+resposta = send_text_via_http(telefone, mensagem)
+print("Resposta ao enviar mensagem via HTTP:")
+print(resposta)     
